3fcf8683ead604ec4f586f2b799d1eb06343ccc5
Pauline Gaget <lupiane@hotmail.com>
2017-04-25 11:08:29 +0200

    removed booking paths from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   resources :tools, only: [ :index, :show, :new, :create ]
+
   resources :bookings, only: [ :create, :update ]
 
-  resources :users , only: [ :show ] do
-    member do
-      get 'bookings', to: 'users#bookings'
-    end
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
