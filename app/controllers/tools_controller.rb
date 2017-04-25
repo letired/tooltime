badb74b9dcf7759db3400b3b71b5c9ae2e41fd68
Spencer Bray <spencerjohnbray@gmail.com>
2017-04-25 16:52:20 +0200

    added a working tool new form and create controller method

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,10 +1,19 @@
 class ToolsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show ]
+  before_action :authenticate_user!, only: [:new]
   def new
+    @tool = Tool.new
   end
 
   def create
-
+    @tool = Tool.new(tool_create_params)
+    @user = User.find(params[:user_id])
+    @tool.user = @user
+    if @tool.save
+      redirect_to @tool
+    else
+      render :new
+    end
   end
 
   def index
@@ -23,5 +32,11 @@ class ToolsController < ApplicationController
   def show
     @tool = Tool.find(params[:id])
     @booking = Booking.new
+  end
+
+  private
+
+  def tool_create_params
+    params.require(:tool).permit(:name, :category, :location, :description, :user_id, :photo)
   end
 end
