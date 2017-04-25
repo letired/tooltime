14871b6e706d9eb3ebb9521f15a885016f205b61
Pauline Gaget <lupiane@hotmail.com>
2017-04-25 11:22:19 +0200

    added password and password confirmation to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,9 @@ puts 'Creating users...'
 10.times do
   user = User.new(first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    email: Faker::Internet.email)
+    email: Faker::Internet.email,
+    password: "password!",
+    password_confirmation: "password!")
   user.save
 end
 
