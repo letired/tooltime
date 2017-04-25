223f043097b459db7f472992e59ad403515f5d10
Patrick Scheuchzer <scheuchzer.patrick@gmail.com>
2017-04-25 11:04:18 +0200

    added users controller with actions 'bookings' and 'tools', created respective paths in the controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   resources :tools, only: [ :index, :show, :new, :create ]
+
+  resources :users , only: [ ] do
+      member do
+        get 'bookings', to: 'users#bookings'
+        get 'tools', to: 'users#tools'
+      end
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
