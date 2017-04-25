b40ffb9c6eac3c062710ec878b3b0a2cc5cbb09c
Pasquale Kress <wabisabisf@gmail.com>
2017-04-25 17:27:28 +0200

    model validations and first and last name added to signup form

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -2,4 +2,9 @@ class Tool < ApplicationRecord
   CATEGORIES = %w(Home\ Improvement Automotive Gardening)
   belongs_to :user
   has_many :bookings
+
+  validates :user_id, presence: true
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :location, presence: true
 end
