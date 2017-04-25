9411191bd17be126c0b6831fbec5a9aef26b7bb3
Patrick Scheuchzer <scheuchzer.patrick@gmail.com>
2017-04-25 15:36:28 +0200

    created both users actions, views only function as placeholders

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
   def bookings
+    if user_signed_in?
+      if current_user.id == params[:id].to_i
+        @bookings = Booking.where(user_id: params[:id])
+      else
+        redirect_to bookings_user_path(current_user)
+      end
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def tools
+    if user_signed_in?
+      if current_user.id == params[:id].to_i
+        @tools = Tool.where(user_id: params[:id])
+      else
+        redirect_to tools_user_path(current_user)
+      end
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
