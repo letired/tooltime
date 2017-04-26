af6fbf4672cdb2a0fbdd1ac72db9009fa2bd61a2
Spencer Bray <spencerjohnbray@gmail.com>
2017-04-26 11:49:00 +0200

    add photo helper for cards to application helper instead of tools

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # allows display of tools in cards or inserts a placeholder
+  def card_photo?(tool)
+    if tool.photo
+      cl_image_path tool.photo.path, height: 300, width: 400, crop: :fill
+    else
+      image_path("https://www.placecage.com/g/300/400")
+    end
+  end
 end
