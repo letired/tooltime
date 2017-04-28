abaede4b2ce181ddb76304766c74ce325d8f82c4
Spencer Bray <spencerjohnbray@gmail.com>
2017-04-28 11:15:35 +0200

    fix tool geocoding, add date to search results

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -2,7 +2,7 @@ class Tool < ApplicationRecord
   CATEGORIES = %w(Home\ Improvement Automotive Gardening)
   CITIES = %w(Berlin Paris London)
   # gecode specific settings
-  geocoded_by :location
+  geocoded_by :address_for_geocoding
   after_validation :geocode, if: :location_changed?
   # db relations
   belongs_to :user
@@ -15,4 +15,11 @@ class Tool < ApplicationRecord
   validates :location, presence: true
   has_attachment :photo
   # belongs_to :owner, class_name: "User", foreign_key: :user_id
+
+  private
+
+  def address_for_geocoding
+    "#{self.location}, #{self.city}"
+  end
+
 end
