a13564ff9dc125d2bed98d8ae51b52abc932f039
Pauline Gaget <lupiane@hotmail.com>
2017-04-28 13:30:30 +0200

    background image no longer repeats itself on cards in tools show

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
 
   def action_card_photo?(tool)
     if tool.photo
-      cl_image_path(tool.photo.path, {width: 150, crop:"scale"})
+      cl_image_path(tool.photo.path, {width: 150, crop:"thumb"})
     else
       image_path("https://www.placecage.com/g/150/100")
     end
